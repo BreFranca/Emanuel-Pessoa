2151630b3eebfbb5c8297d3ee0645100516331c7
Breno Franca <bre.sfranca@gmail.com>
2018-06-14 00:20:55 -0300

    Insert Animations

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,13 @@
 $('nav ul li a').click(function(e) {
-	e.preventDefault;
+	e.preventDefault();
+
+	href = $(this).attr('href');
+
+	$('#content').addClass('open');
+
+	$('nav ul li').removeClass('active');
+	$(this).parent().addClass('active');
+
+	$('.content').hide();
+	$('.content.'+ href).fadeIn();
 });
